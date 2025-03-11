54c1c3679769f0113c1a60e79d300ec5356c7986
nichita <nichita.tonu@orizont.md>
2025-03-11 18:07:37 +0200

    dom terminat

--- js/script.js
+++ js/script.js
@@ -11,55 +11,55 @@ const request = async() => {
                 console.log(element);
                 const body = document.body;
 
-                const wrapper = creareDiv;
+                const wrapper = document.createElement('div');
                 wrapper.classList.add('wrapper');
                 body.append(wrapper);
 
-                const ambalaj = creareDiv;
+                const ambalaj = document.createElement('div');
                 ambalaj.classList.add('ambalaj');
                 wrapper.append(ambalaj);
 
                 // patrate galbene
-                const line1 = creareDiv;
+                const line1 = document.createElement('div');
                 line1.classList.add('line1');
                 wrapper.append(line1);
 
-                const line2 = creareDiv;
+                const line2 = document.createElement('div');
                 line2.classList.add('line2');
                 wrapper.append(line2);
 
-                const line3 = creareDiv;
+                const line3 = document.createElement('div');
                 line3.classList.add('line3');
                 wrapper.append(line3);
 
                 // header rosii
-                const header1 = creareDiv;
+                const header1 = document.createElement('div');
                 header1.classList.add('header1');
                 header1.textContent = 'ID';
                 line1.append(header1);
 
-                const header2 = creareDiv;
+                const header2 = document.createElement('div');
                 header2.classList.add('header2');
                 header2.textContent = 'KIND';
                 line2.append(header2);
 
-                const header3 = creareDiv;
+                const header3 = document.createElement('div');
                 header3.classList.add('header3');
                 header3.textContent = 'BODY';
                 line3.append(header3);
 
                 // patrate albastre - content
-                const content1 = creareDiv;
+                const content1 = document.createElement('div');
                 content1.classList.add('content1');
                 content1.textContent = element.id;
                 line1.append(content1);
 
-                const content2 = creareDiv;
+                const content2 = document.createElement('div');
                 content2.classList.add('content2');
                 content2.textContent = element.kind;
                 line2.append(content2);
 
-                const content3 = creareDiv;
+                const content3 = document.createElement('div');
                 content3.classList.add('content3');
                 content3.textContent = element.volumeInfo.language;
                 content3.textContent = element.volumeInfo.title;
@@ -85,6 +85,3 @@ request()
 
 
 
-const creareDiv = () =>{
-    const div = document.createElement('div')
-}
